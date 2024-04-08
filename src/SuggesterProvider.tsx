1f781a0032af265e776f242c69cb24df4e8a5f4b
noqcks <benji@093b.org>
2024-04-08 02:34:35 +0000

    Upgrade dependencies

--- src/SuggesterProvider.tsx
+++ src/SuggesterProvider.tsx
@@ -60,7 +60,8 @@ export class SuggesterProvider extends Component<
       threshold: 0.6,
       location: 0,
       distance: 100,
-      maxPatternLength: 32,
+      // The 'maxPatternLength' option was removed or is not available in the current Fuse.js version
+      // maxPatternLength: 32,
       minMatchCharLength: 1,
       keys: ['value'],
     })
